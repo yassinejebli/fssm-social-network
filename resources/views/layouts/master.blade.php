7556b92bb3029e89f093104ec0337dc35a9974d7
yassinejebli <jebli.yassine@gmail.com>
2017-01-05 13:02:59 +0000

    a2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
 <!-- Main navbar -->
 <div class="navbar navbar-inverse bg-indigo">
     <div class="navbar-header">
-        <a class="navbar-brand" href="{{ route('index') }}"><img src="../assets/images/logo_light.png" alt=""></a>
+        <a class="navbar-brand" href="{{ route('index') }}"><img src="" alt="">IT-TEAM</a>
 
         <ul class="nav navbar-nav visible-xs-block">
             <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
