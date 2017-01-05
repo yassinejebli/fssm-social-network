707b7e86434e7372ca1789c88dfc25a93e124d2f
yassinejebli <jebli.yassine@gmail.com>
2017-01-05 12:38:16 +0000

    a2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -914,7 +914,7 @@
                         for (var i = 0; i < files.length; i++) {
                             var file = files[i];
                             $upload.upload({
-                                url: "{{ route('chargerImage') }}",
+                                url: "{{ route('ajouterPublication') }}",
                                 //fields: { _token : '{{ csrf_token() }}' },
                                 file: file
                             }).progress(function (evt) {
